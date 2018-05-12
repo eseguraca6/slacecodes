1f11c8f919165f955b3fa39058de7946ab3f3946
eseguraca6 <eseguraca@github.com>
2018-05-11 18:00:17 -0700

    var

--- FACET_model_current/wavelength_runs/algo.py
+++ FACET_model_current/wavelength_runs/algo.py
@@ -329,12 +329,18 @@ def feedback_method(file, low_angle, high_angle, run_num):
         print("input random:", curr_var)
         #fix this.
         curr_fix = algo_fix(file)
-        np.savetxt('var-'+ str(i)+'.csv', list(zip(curr_fix[0], curr_fix[1], curr_fix[2],
+        np.savetxt(str(low_angle)+"-"+str(high_angle)+'var-'+ str(i)+'.csv', list(zip(curr_fix[0], curr_fix[1], curr_fix[2],
                    curr_fix[3], curr_fix[4], curr_fix[5], curr_fix[6], curr_fix[7])))
-        np.savetxt('inputvar'+str(i)+'.csv',curr_var)
+        np.savetxt(str(low_angle)+"-"+str(high_angle)+'-inputvar'+str(i)+'.csv',curr_var)
         
-feedback_method(file, -5,5,2)
-        
+feedback_method(file, -2,0,2)
+feedback_method(file,  0,2,2)
+feedback_method(file,  3,5,2)
+feedback_method(file, -3,-5,2)
+
+feedback_method(file, -10,-5,2)
+feedback_method(file, 5,10,2)
+feedback_method(file, 10,20, 2)
     
 """
  for i in range(0, run_num):
