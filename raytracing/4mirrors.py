bc9032794a8bb34b58d815b85374faa7f67e65a5
eseguraca6 <eseguraca@github.com>
2018-07-31 13:56:23 -0700

    newdata

--- raytracing/4mirrors.py
+++ raytracing/4mirrors.py
@@ -207,26 +207,18 @@ def algo_facet2_var(file, var_arr):
     h_var = var_arr[3]
     var4x = np.random.uniform(l_var, h_var)
     var4y = np.random.uniform(l_var, h_var)
-    h_var = var_arr[4]
-    var5x = np.random.uniform(l_var, h_var)
-    var5y = np.random.uniform(l_var, h_var)
 
-    var6x = np.random.uniform(l_var, h_var)
-    var6y = np.random.uniform(l_var, h_var)
-    print('variations inputs:')
     vec = np.matrix([ 
             [var1x],
             [var1y],
             [var2x],
             [var2y],
             [var3x],
-            [var3y],
+            [var3y]])
+    """
             [var4x],
-            [var4y],
-            [var5x],
-            [var5y],
-            [var6x],
-            [var6y]])
+            [var4y]])
+    """
     print(vec)
     #var M1
     surface_control_xvar(file, 3, var1x)
@@ -238,14 +230,8 @@ def algo_facet2_var(file, var_arr):
     surface_control_xvar(file, 21, var3x)
     surface_control_yvar(file, 21, var3y)
     
-    surface_control_xvar(file, 30, var4x)
-    surface_control_yvar(file, 30, var4y)
-    
-    surface_control_xvar(file, 39, var5x)
-    surface_control_yvar(file, 39, var5y)
-    
-    surface_control_xvar(file, 48, var6x)
-    surface_control_yvar(file, 48, var6y)
+    #surface_control_xvar(file, 30, var4x)
+    #surface_control_yvar(file, 30, var4y)
     print('variations finished')
     pyz.closeLink()
     np.savetxt(r'C:\Users\pwfa-facet2\Desktop\slacecodes\raytracing\f2beamvarmod.csv', vec)
@@ -262,47 +248,42 @@ def f_block(cx, cy, rot, d_t):
 
 optics_deg = [45, 0, 90,
               -45, 0, -90,
-              -45, 0, -90, 
-              45, 0, 90, 
-              -45,0, -90,
-              -45,0,-90]
+              -45, 0, -90]
 
 def f_beamline(config_optics):
     m1_mat = f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2068)
     m2_mat = f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082)
     m3_mat = f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121)
-    m4_mat = f_block(optics_deg[9], optics_deg[10], optics_deg[11], 2394)
-    m5_mat = f_block(optics_deg[12], optics_deg[13], optics_deg[14], 11738.3)
-    m6_mat = f_block(optics_deg[15], optics_deg[16], optics_deg[17], 3000)
+    #m4_mat = f_block(optics_deg[9], optics_deg[10], optics_deg[11], 2394)
+    #m5_mat = f_block(optics_deg[12], optics_deg[13], optics_deg[14], 11738.3)
+    #m6_mat = f_block(optics_deg[15], optics_deg[16], optics_deg[17], 3000)
     
     m1xtm2 = f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082)
     m1xtm3 =  f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082+6121)
-    m1xtm4 =  f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082+6121+2394)
-    m1xtm5 =  f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082+6121+2394+11738.3)
-    m1xtm6 =  f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082+6121+2394+11738.3+3000)
+    #m1xtm4 =  f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082+6121+2394)
+    #m1xtm5 =  f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082+6121+2394+11738.3)
+    #m1xtm6 =  f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082+6121+2394+11738.3+3000)
     
     m2xtm3 =  f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082+6121)
-    m2xtm4 =  f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082+6121+2394)
-    m2xtm5 =  f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082+6121+2394+11738.3)
-    m2xtm6 =  f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082+6121+2394+11738.3+3000)
+    #m2xtm4 =  f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082+6121+2394)
+    #m2xtm5 =  f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082+6121+2394+11738.3)
+    #m2xtm6 =  f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082+6121+2394+11738.3+3000)
 
-    m3xtm4 =  f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121+2394)
-    m3xtm5 =  f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121+2394+11738.3)
-    m3xtm6 =  f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121+2394+11738.3+3000)
+    #m3xtm4 =  f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121+2394)
+    #m3xtm5 =  f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121+2394+11738.3)
+    #m3xtm6 =  f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121+2394+11738.3+3000)
     
-    m4xtm5 = f_block(optics_deg[9], optics_deg[10], optics_deg[11], 2394+11738.3)
-    m4xtm6 = f_block(optics_deg[9], optics_deg[10], optics_deg[11], 2394+11738.3+3000)
+    #m4xtm5 = f_block(optics_deg[9], optics_deg[10], optics_deg[11], 2394+11738.3)
+    #m4xtm6 = f_block(optics_deg[9], optics_deg[10], optics_deg[11], 2394+11738.3+3000)
     
-    m5xtm6 = f_block(optics_deg[12], optics_deg[13], optics_deg[14], 11738.3+3000)
+    #m5xtm6 = f_block(optics_deg[12], optics_deg[13], optics_deg[14], 11738.3+3000)
     
     z = np.zeros([2,2])
     
-    full_matrix = np.bmat( [ [m1_mat, z, z,z,z,z],
-                             [m1xtm2, m2_mat, z,z,z,z], 
-                             [m1xtm3, m2xtm3, m3_mat, z,z,z],
-                             [m1xtm4, m2xtm4, m3xtm4, m4_mat, z,z],
-                             [m1xtm5, m2xtm5, m3xtm5, m4xtm5, m5_mat,z], 
-                             [m1xtm6, m2xtm6, m3xtm6, m4xtm6, m5xtm6, m6_mat]])
+    full_matrix = np.bmat( [ [m1_mat, z, z],
+                             [m1xtm2, m2_mat, z], 
+                             [m1xtm3, m2xtm3, m3_mat]])
+                             #[m1xtm4, m2xtm4, m3xtm4, m4_mat]])
 
     return(full_matrix)    
     
@@ -318,22 +299,15 @@ def ccd_screens(file):
     ccd3x = link.zOperandValue('POPD', 28, 1, 0, 11)
     ccd3y = link.zOperandValue('POPD', 28, 1, 0, 12)
     
-    ccd4x = link.zOperandValue('POPD', 37, 1, 0, 11)
-    ccd4y = link.zOperandValue('POPD', 37, 1, 0, 12)
-    
-    ccd5x = link.zOperandValue('POPD', 46, 1, 0, 11)
-    ccd5y = link.zOperandValue('POPD', 46, 1, 0, 12)
+    #ccd4x = link.zOperandValue('POPD', 37, 1, 0, 11)
+    #ccd4y = link.zOperandValue('POPD', 37, 1, 0, 12)
 
-    ccd6x = link.zOperandValue('POPD', 55, 1, 0, 11)
-    ccd6y = link.zOperandValue('POPD', 55, 1, 0, 12)
     
     
     beam_pos_vec = np.matrix([ [ccd1x], [ccd1y],
                               [ccd2x], [ccd2y],
-                              [ccd3x], [ccd3y],
-                              [ccd4x], [ccd4y],
-                              [ccd5x], [ccd5y],
-                              [ccd6x], [ccd6y]])
+                              [ccd3x], [ccd3y]])
+
     pyz.closeLink()
     return(beam_pos_vec)
     
@@ -352,12 +326,9 @@ def algo_fix(file):
     beam_2y =[]
     beam_3x =[]
     beam_3y =[]
-    beam_4x =[]
-    beam_4y =[]
-    beam_5x =[]
-    beam_5y =[]
-    beam_6x =[]
-    beam_6y =[]
+    #beam_4x =[]
+    #beam_4y =[]
+
     
     v_1x =[]
     v_1y =[]
@@ -365,12 +336,9 @@ def algo_fix(file):
     v_2y =[]
     v_3x =[]
     v_3y =[]
-    v_4x =[]
-    v_4y =[]
-    v_5x =[]
-    v_5y =[]
-    v_6x =[]
-    v_6y =[]
+    #v_4x =[]
+    #v_4y =[]
+
 
     #check for misalignments 
     beam_mem =[] 
@@ -390,14 +358,8 @@ def algo_fix(file):
     beam_3x.append(curr_beam_pos.item(4))
     beam_3y.append(curr_beam_pos.item(5))
     
-    beam_4x.append(curr_beam_pos.item(6))
-    beam_4y.append(curr_beam_pos.item(7))
-    
-    beam_5x.append(curr_beam_pos.item(8))
-    beam_5y.append(curr_beam_pos.item(9))
-    
-    beam_6x.append(curr_beam_pos.item(10))
-    beam_6y.append(curr_beam_pos.item(11))
+    #beam_4x.append(curr_beam_pos.item(6))
+    #beam_4y.append(curr_beam_pos.item(7))
     
     delta_dev = []
     #check for the first variations 
@@ -410,12 +372,9 @@ def algo_fix(file):
     c_2y = misalign_vec.item(3)
     c_3x = misalign_vec.item(4)
     c_3y = misalign_vec.item(5)
-    c_4x = misalign_vec.item(6)
-    c_4y = misalign_vec.item(7)
-    c_5x = misalign_vec.item(8)
-    c_5y = misalign_vec.item(9)
-    c_6x = misalign_vec.item(10)
-    c_6y = misalign_vec.item(11)
+    #c_4x = misalign_vec.item(6)
+    #c_4y = misalign_vec.item(7)
+
             
     v_1x.append(c_1x)
     v_1y.append(c_1y)
@@ -423,13 +382,10 @@ def algo_fix(file):
     v_2y.append(c_2y)
     v_3x.append(c_3x)
     v_3y.append(c_3y)
-    v_4x.append(c_4x)
-    v_4y.append(c_4y)
+    #v_4x.append(c_4x)
+    #v_4y.append(c_4y)
             
-    v_5x.append(c_5x)
-    v_5y.append(c_5y)
-    v_6x.append(c_6x)
-    v_6y.append(c_6y)
+
     #feed those variations
     surface_control_xcorr(file, 4, -c_1x)
     surface_control_ycorr(file, 4, -c_1y)
@@ -440,20 +396,14 @@ def algo_fix(file):
     surface_control_xcorr(file, 22, -c_3x)
     surface_control_ycorr(file, 22, -c_3y)
                 
-    surface_control_xcorr(file, 31, -c_4x)
-    surface_control_ycorr(file, 31, -c_4y)
+    #surface_control_xcorr(file, 31, -c_4x)
+    #surface_control_ycorr(file, 31, -c_4y)
     
-    surface_control_xcorr(file, 40, -c_5x)
-    surface_control_ycorr(file, 40, -c_5y)
-    
-    surface_control_xcorr(file, 49, -c_6x)
-    surface_control_ycorr(file, 49, -c_6y)
+
             
     
     #input integral element
     corr_mem.append(misalign_vec)
-    delta_dev.append(misalign_vec)
-    it = it+1
     
     curr_beam_pos = ccd_screens(file)
     print("after changes misaligned vector:")
@@ -479,14 +429,8 @@ def algo_fix(file):
         beam_3x.append(curr_beam_pos.item(4))
         beam_3y.append(curr_beam_pos.item(5))
     
-        beam_4x.append(curr_beam_pos.item(6))
-        beam_4y.append(curr_beam_pos.item(7))
-    
-        beam_5x.append(curr_beam_pos.item(8))
-        beam_5y.append(curr_beam_pos.item(9))
-    
-        beam_6x.append(curr_beam_pos.item(10))
-        beam_6y.append(curr_beam_pos.item(11))
+        #beam_4x.append(curr_beam_pos.item(6))
+        #beam_4y.append(curr_beam_pos.item(7))
     
         nccd1x = curr_beam_pos.item(0)
         nccd1y = curr_beam_pos.item(1)
@@ -497,14 +441,9 @@ def algo_fix(file):
         nccd3x =  curr_beam_pos.item(4)
         nccd3y =  curr_beam_pos.item(5)
         
-        nccd4x =  curr_beam_pos.item(6)
-        nccd4y =  curr_beam_pos.item(7)
-    
-        nccd5x = curr_beam_pos.item(8)
-        nccd5y =  curr_beam_pos.item(9)
+        #nccd4x =  curr_beam_pos.item(6)
+        #nccd4y =  curr_beam_pos.item(7)
 
-        nccd6x =  curr_beam_pos.item(10)
-        nccd6y =  curr_beam_pos.item(11)
         
         diff1x = nccd1x
         diff1y = nccd1y
@@ -512,12 +451,10 @@ def algo_fix(file):
         diff2y = nccd2y
         diff3x = nccd3x
         diff3y = nccd3y
+        """
         diff4x = nccd4x
         diff4y = nccd4y
-        diff5x = nccd5x
-        diff5y = nccd5y
-        diff6x = nccd6x
-        diff6y = nccd6y
+        """
         
         #check misalignment
         if diff1x < 0.01 and \
@@ -525,13 +462,7 @@ def algo_fix(file):
            diff2x < 0.01 and \
            diff2y < 0.01 and \
            diff3x < 0.01 and \
-           diff3y < 0.01 and \
-           diff4x < 0.01 and \
-           diff4y < 0.01 and \
-           diff5x < 0.01 and \
-           diff5y < 0.01 and \
-           diff6x < 0.01 and \
-           diff6y < 0.01:
+           diff3y < 0.01: 
             status = 'done'
             print(status)
             pyz.closeLink()
@@ -545,11 +476,11 @@ def algo_fix(file):
             print('current variations')
             print(misalign_vec)
             #append elements
-            print(corr_mem)
+            #print(corr_mem)
 
-            c_vec = (1)*misalign_vec + ((1/10)*corr_mem[it-1])
+            c_vec = (1/10)*misalign_vec + ((-1/500)*corr_mem[it])
             corr_mem.append(c_vec)
-            print(corr_mem)
+            #print(corr_mem)
 
             #derivative_comp = c_vec - delta_dev[it-1]
             #c_vec = c_vec + (1/200)*derivative_comp
@@ -563,12 +494,9 @@ def algo_fix(file):
             c_2y = c_vec.item(3)
             c_3x = c_vec.item(4)
             c_3y = c_vec.item(5)
-            c_4x = c_vec.item(6)
-            c_4y = c_vec.item(7)
-            c_5x = c_vec.item(8)
-            c_5y = c_vec.item(9)
-            c_6x = c_vec.item(10)
-            c_6y = c_vec.item(11)
+            #c_4x = c_vec.item(6)
+            #c_4y = c_vec.item(7)
+
             
             v_1x.append(c_1x)
             v_1y.append(c_1y)
@@ -576,12 +504,9 @@ def algo_fix(file):
             v_2y.append(c_2y)
             v_3x.append(c_3x)
             v_3y.append(c_3y)
-            v_4x.append(c_4x)
-            v_4y.append(c_4y)
-            v_5x.append(c_5x)
-            v_5y.append(c_5y)
-            v_6x.append(c_6x)
-            v_6y.append(c_6y)
+            #v_4x.append(c_4x)
+            #v_4y.append(c_4y)
+
             
             #execute correction
             surface_control_xcorr(file, 4, -c_1x)
@@ -593,25 +518,20 @@ def algo_fix(file):
             surface_control_xcorr(file, 22, -c_3x)
             surface_control_ycorr(file, 22, -c_3y)
                 
-            surface_control_xcorr(file, 31, -c_4x)
-            surface_control_ycorr(file, 31, -c_4y)
-    
-            surface_control_xcorr(file, 40, -c_5x)
-            surface_control_ycorr(file, 40, -c_5y)
-    
-            surface_control_xcorr(file, 49, -c_6x)
-            surface_control_ycorr(file, 49, -c_6y)
+            #surface_control_xcorr(file, 31, -c_4x)
+            #surface_control_ycorr(file, 31, -c_4y)
+
             
             it=it+1
         
     np.savetxt(r'C:\Users\pwfa-facet2\Desktop\slacecodes\raytracing\f2beamtrackmod.csv', \
                list(zip(beam_1x, beam_1y, beam_2x, beam_2y, \
-                        beam_3x, beam_3y, beam_4x, beam_4y, \
-                        beam_5x, beam_5y, beam_6x, beam_6y)))
+                        beam_3x, beam_3y \
+                       )))
     np.savetxt(r'C:\Users\pwfa-facet2\Desktop\slacecodes\raytracing\f2vartrackmod.csv', \
                list(zip(v_1x, v_1y, v_2x, v_2y, \
-                        v_3x, v_3y, v_4x, v_4y, \
-                        v_5x, v_5y, v_6x, v_6y)))
+                        v_3x, v_3y \
+                        )))
     
             
             
@@ -623,4 +543,3 @@ def algo_fix(file):
 config_simulation(file, configuration_angles)
 algo_facet2_var(file,var_vec)
 algo_fix(file)
-
