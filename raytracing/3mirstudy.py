75aae152dccbdadbf32df6306aa5bcac94e112f8
eseguraca6 <eseguraca@github.com>
2018-08-01 15:40:31 -0700

    newdata

--- raytracing/3mirstudy.py
+++ raytracing/3mirstudy.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul 20 12:46:46 2018
+Created on Wed Aug  1 15:08:55 2018
 
 @author: pwfa-facet2
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
@@ -166,105 +165,9 @@ def surface_control_yvar(file, surface_num, val):
     link.zSetSurfaceParameter(surface_num+4, 4, -val)
     link.zSaveFile(file)  
     pyz.closeLink()
-    
-def algo_facet2_var(file, var_arr):
-    link = pyz.createLink()
-    link.zLoadFile(file)
-    l_var = -var_arr[0]
-    h_var = var_arr[0]
-    var1x = np.random.uniform(l_var, h_var)
-    var1y = np.random.uniform(l_var, h_var)
-    l_var = -var_arr[1]
-    h_var = var_arr[1]
-    var2x = np.random.uniform(l_var, h_var)
-    var2y = np.random.uniform(l_var, h_var)
-    h_var = var_arr[2]
-    l_var = -var_arr[2]
-    var3x = np.random.uniform(l_var, h_var)
-    var3y = np.random.uniform(l_var, h_var)
-    #h_var = var_arr[3]
-    #var4x = np.random.uniform(l_var, h_var)
-    #var4y = np.random.uniform(l_var, h_var)
 
-    vec = np.matrix([ 
-            [var1x],
-            [var1y],
-            [var2x],
-            [var2y],
-            [var3x],
-            [var3y]])
-    """
-            [var4x],
-            [var4y]])
-    """
-    print(vec)
-    #var M1
-    surface_control_xvar(file, 3, var1x)
-    surface_control_yvar(file, 3, var1y)
-    
-    surface_control_xvar(file, 12, var2x)
-    surface_control_yvar(file, 12, var2y)
-    
-    surface_control_xvar(file, 21, var3x)
-    surface_control_yvar(file, 21, var3y)
-    
-    #surface_control_xvar(file, 30, var4x)
-    #surface_control_yvar(file, 30, var4y)
-    print('variations finished')
-    pyz.closeLink()
-    np.savetxt(r'C:\Users\pwfa-facet2\Desktop\slacecodes\raytracing\f2beam-org-var.csv', vec)
-    
-    
-def f_block(cx, cy, rot, d_t):
-    cx, cy, rot = np.deg2rad(cx),np.deg2rad(cy),np.deg2rad(rot)
-    drift = np.matrix([ [d_t, 0], [0, d_t] ])
-    axis = np.matrix([ [2*np.cos(cx), 0], [0, 2*np.cos(cy)] ])
-    rot_m = np.matrix([ [np.cos(rot), -np.sin(rot)], [np.sin(rot), np.cos(rot)] ])
-    r = np.linalg.multi_dot([ drift, axis, rot_m])
-    return(r)
+beam_pos =[]
 
-
-optics_deg = [45, 0, 90,
-              -45, 0, -90,
-              -45, 0, -90]
-
-def f_beamline(config_optics):
-    m1_mat = f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2068)
-    m2_mat = f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082)
-    m3_mat = f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121)
-    #m4_mat = f_block(optics_deg[9], optics_deg[10], optics_deg[11], 2394)
-    #m5_mat = f_block(optics_deg[12], optics_deg[13], optics_deg[14], 11738.3)
-    #m6_mat = f_block(optics_deg[15], optics_deg[16], optics_deg[17], 3000)
-    
-    m1xtm2 = f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082)
-    m1xtm3 =  f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082+6121)
-    #m1xtm4 =  f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082+6121+2394)
-    #m1xtm5 =  f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082+6121+2394+11738.3)
-    #m1xtm6 =  f_block(optics_deg[0], optics_deg[1], optics_deg[2], 2095+3082+6121+2394+11738.3+3000)
-    
-    m2xtm3 =  f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082+6121)
-    #m2xtm4 =  f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082+6121+2394)
-    #m2xtm5 =  f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082+6121+2394+11738.3)
-    #m2xtm6 =  f_block(optics_deg[3], optics_deg[4], optics_deg[5], 3082+6121+2394+11738.3+3000)
-
-    #m3xtm4 =  f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121+2394)
-    #m3xtm5 =  f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121+2394+11738.3)
-    #m3xtm6 =  f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121+2394+11738.3+3000)
-    
-    #m4xtm5 = f_block(optics_deg[9], optics_deg[10], optics_deg[11], 2394+11738.3)
-    #m4xtm6 = f_block(optics_deg[9], optics_deg[10], optics_deg[11], 2394+11738.3+3000)
-    
-    #m5xtm6 = f_block(optics_deg[12], optics_deg[13], optics_deg[14], 11738.3+3000)
-    
-    z = np.zeros([2,2])
-    
-    full_matrix = np.bmat( [ [m1_mat, z, z],
-                             [m1xtm2, m2_mat, z], 
-                             [m1xtm3, m2xtm3, m3_mat]])
-                             #[m1xtm4, m2xtm4, m3xtm4, m4_mat]])
-
-    return(full_matrix)    
-m3_mat = f_block(optics_deg[6], optics_deg[7], optics_deg[8], 6121)  
 def ccd_screens(file):
     link = pyz.createLink()
     link.zLoadFile(file)
@@ -289,46 +192,24 @@ def ccd_screens(file):
     pyz.closeLink()
     return(beam_pos_vec)
     
-f_beamline(optics_deg)
-def algo_fix(file):
-    link= pyz.createLink()
-    link.zLoadFile(file)
-      
-    imax = 20
-    corr_mem = []
-    it = 0
+r = np.arange(-1,1.1,0.1)
+beam_1x =[]
+beam_1y =[]
+beam_2x =[]
+beam_2y =[]
+beam_3x =[]
+beam_3y =[]
+
+config_simulation(file, configuration_angles)
+for i in r:
+    surface_control_xvar(file,3,i)
+    surface_control_xvar(file, 12,i)
+    surface_control_xvar(file, 21,i)
     
-    beam_1x =[]
-    beam_1y =[]
-    beam_2x =[]
-    beam_2y =[]
-    beam_3x =[]
-    beam_3y =[]
-    #beam_4x =[]
-    #beam_4y =[]
-
-    
-    v_1x =[]
-    v_1y =[]
-    v_2x =[]
-    v_2y =[]
-    v_3x =[]
-    v_3y =[]
-    #v_4x =[]
-    #v_4y =[]
-
-
-    #check for misalignments 
-    beam_mem =[] 
-    status = 'not done'
-    finv = np.linalg.inv(f_beamline(optics_deg))
+    surface_control_yvar(file,3,i)
+    surface_control_yvar(file, 12,i)
+    surface_control_yvar(file, 21,i)
     curr_beam_pos = ccd_screens(file)
-    print("start misaligned vector:")
-    print(curr_beam_pos)
-    beam_mem.append(curr_beam_pos)
-    np.savetxt(r'C:\Users\pwfa-facet2\Desktop\slacecodes\raytracing\1stmisalignedbeam.csv', curr_beam_pos)
-
-    
     beam_1x.append(curr_beam_pos.item(0))
     beam_1y.append(curr_beam_pos.item(1))
     
@@ -337,53 +218,11 @@ def algo_fix(file):
     
     beam_3x.append(curr_beam_pos.item(4))
     beam_3y.append(curr_beam_pos.item(5))
-    
-    #beam_4x.append(curr_beam_pos.item(6))
-    #beam_4y.append(curr_beam_pos.item(7))
-    
-    
-    #check for the first variations 
-    misalign_vec = np.rad2deg(np.matmul(finv, curr_beam_pos))
-    print('current variations')
-    print(misalign_vec)    
-    np.savetxt(r'C:\Users\pwfa-facet2\Desktop\slacecodes\raytracing\f2beamtrackmod-1st.csv', misalign_vec)
-    c_1x = misalign_vec.item(0)
-    c_1y = misalign_vec.item(1)
-    c_2x = misalign_vec.item(2)
-    c_2y = misalign_vec.item(3)
-    c_3x = misalign_vec.item(4)
-    c_3y = misalign_vec.item(5)
-            
-    #execute variations 
-    surface_control_xcorr(file, 4, -c_1x)
-    surface_control_ycorr(file, 4, -c_1y)
-    
-    surface_control_xcorr(file, 13, -c_2x)
-    surface_control_ycorr(file, 13, -c_2y)
-    
-    #iterate two mirrors 
-    status = 'not done'
-    while status != 'done'
-    
-    print('after correction vector:')
-    vec = ccd_screens(file)
-    print(vec)
-    m3_beam_vec = np.matrix([ [vec.item(4)], [vec.item(5)] ])
-    m3inv = np.linalg.inv(m3_mat)
-    m3_var_vec = np.rad2deg(np.matmul(m3inv, m3_beam_vec))
-    print('m3 var:')
-    print(m3_var_vec)
-    
 
+    print('not done')
     
+print('finally done')
+np.savetxt(r'C:\Users\pwfa-facet2\Desktop\slacecodes\raytracing\m3char.csv', \
+               list(zip(beam_1x, beam_1y, beam_2x, beam_2y, \
+                        beam_3x, beam_3y)))
     
-    
-    
-    pyz.closeLink()
-            
-
-                    
-    
-config_simulation(file, configuration_angles)
-algo_facet2_var(file,var_vec)
-algo_fix(file)
